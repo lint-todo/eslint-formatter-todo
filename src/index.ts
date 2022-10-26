7b792cc6db450f8cfafa9b07afd7c80b20b12a99
Robert Jackson <me@rwjblue.com>
2022-10-26 16:30:10 -0400

    Ensure correct `eslint` version is being used. (#397)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { formatter } from './formatter';
+import validatePeerDependencies from 'validate-peer-dependencies';
+
+validatePeerDependencies(__dirname);
 
 // special TS syntax: https://github.com/microsoft/TypeScript/issues/2719
 export = formatter;
